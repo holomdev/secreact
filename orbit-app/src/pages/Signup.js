5d72a030f9657e05d19c9106816c9794de259827
holomdev <holom.dev@gmail.com>
2021-04-12 13:35:17 -0500

    Complete the User Signup Form

--- orbit-app/src/pages/Signup.js
+++ orbit-app/src/pages/Signup.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Form, Formik } from 'formik';
+import React, {useState} from 'react';
+import {Form, Formik} from 'formik';
 import * as Yup from 'yup';
 import Card from '../components/common/Card';
 import GradientButton from '../components/common/GradientButton';
@@ -10,6 +10,8 @@ import GradientBar from './../components/common/GradientBar';
 import FormError from './../components/FormError';
 import FormSuccess from './../components/FormSuccess';
 import logo from './../images/logo.png';
+import {publicFetch} from "../util/fetch";
+import {Redirect} from "react-router";
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string().required(
@@ -26,13 +28,20 @@ const Signup = () => {
   const [signupSuccess, setSignupSuccess] = useState();
   const [signupError, setSignupError] = useState();
   const [loginLoading, setLoginLoading] = useState(false);
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false);
 
   const submitCredentials = async credentials => {
     try {
       setLoginLoading(true);
+      const {data} = await publicFetch.post('signup', credentials);
+      setSignupSuccess(data.message);
+      setSignupError('');
+      setTimeout(() => {
+        setRedirectOnLogin(true);
+      }, 2000);
     } catch (error) {
       setLoginLoading(false);
-      const { data } = error.response;
+      const {data} = error.response;
       setSignupError(data.message);
       setSignupSuccess('');
     }
@@ -40,21 +49,22 @@ const Signup = () => {
 
   return (
     <>
+      {redirectOnLogin && <Redirect to="/dashboard"/>}
       <section className="w-1/2 h-screen m-auto p-8 sm:pt-10">
-        <GradientBar />
+        <GradientBar/>
         <Card>
           <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full">
               <div>
                 <div className="w-32 m-auto mb-6">
-                  <img src={logo} alt="Logo" />
+                  <img src={logo} alt="Logo"/>
                 </div>
                 <h2 className="mb-2 text-center text-3xl leading-9 font-extrabold text-gray-900">
                   Sign up for an account
                 </h2>
                 <p className="text-gray-600 text-center">
                   Already have an account?{' '}
-                  <Hyperlink to="login" text="Log in now" />
+                  <Hyperlink to="login" text="Log in now"/>
                 </p>
               </div>
               <Formik
@@ -72,10 +82,10 @@ const Signup = () => {
                 {() => (
                   <Form className="mt-8">
                     {signupSuccess && (
-                      <FormSuccess text={signupSuccess} />
+                      <FormSuccess text={signupSuccess}/>
                     )}
                     {signupError && (
-                      <FormError text={signupError} />
+                      <FormError text={signupError}/>
                     )}
                     <input
                       type="hidden"
@@ -86,7 +96,7 @@ const Signup = () => {
                       <div className="flex">
                         <div className="mb-2 mr-2 w-1/2">
                           <div className="mb-1">
-                            <Label text="First Name" />
+                            <Label text="First Name"/>
                           </div>
                           <FormInput
                             ariaLabel="First Name"
@@ -97,7 +107,7 @@ const Signup = () => {
                         </div>
                         <div className="mb-2 ml-2 w-1/2">
                           <div className="mb-1">
-                            <Label text="Last Name" />
+                            <Label text="Last Name"/>
                           </div>
                           <FormInput
                             ariaLabel="Last Name"
@@ -109,7 +119,7 @@ const Signup = () => {
                       </div>
                       <div className="mb-2">
                         <div className="mb-1">
-                          <Label text="Email address" />
+                          <Label text="Email address"/>
                         </div>
                         <FormInput
                           ariaLabel="Email address"
@@ -120,7 +130,7 @@ const Signup = () => {
                       </div>
                       <div>
                         <div className="mb-1">
-                          <Label text="Password" />
+                          <Label text="Password"/>
                         </div>
                         <FormInput
                           ariaLabel="Password"
