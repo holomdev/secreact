ec7f2d497dec82b0d0ae264c9ed6766bd5931b31
holomdev <holom.dev@gmail.com>
2021-04-12 13:57:36 -0500

    Complete the User Login Form

--- orbit-app/src/pages/Login.js
+++ orbit-app/src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Form, Formik } from 'formik';
+import React, {useState} from 'react';
+import {Form, Formik} from 'formik';
 import * as Yup from 'yup';
 import Card from '../components/common/Card';
 import Hyperlink from './../components/common/Hyperlink';
@@ -10,6 +10,8 @@ import FormError from './../components/FormError';
 import GradientBar from './../components/common/GradientBar';
 import GradientButton from '../components/common/GradientButton';
 import logo from './../images/logo.png';
+import {publicFetch} from "../util/fetch";
+import {Redirect} from "react-router";
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().required('Email is required'),
@@ -20,13 +22,20 @@ const Login = () => {
   const [loginSuccess, setLoginSuccess] = useState();
   const [loginError, setLoginError] = useState();
   const [loginLoading, setLoginLoading] = useState(false);
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false);
 
   const submitCredentials = async credentials => {
     try {
       setLoginLoading(true);
+      const {data} = await publicFetch.post('authenticate', credentials);
+      setLoginSuccess(data.message);
+      setLoginError('');
+      setTimeout(() => {
+        setRedirectOnLogin(true);
+      }, 2000);
     } catch (error) {
       setLoginLoading(false);
-      const { data } = error.response;
+      const {data} = error.response;
       setLoginError(data.message);
       setLoginSuccess(null);
     }
@@ -34,14 +43,15 @@ const Login = () => {
 
   return (
     <>
+      {redirectOnLogin && <Redirect to="/dashboard"/>}
       <section className="w-full sm:w-1/2 h-screen m-auto p-8 sm:pt-10">
-        <GradientBar />
+        <GradientBar/>
         <Card>
           <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full">
               <div>
                 <div className="w-32 m-auto mb-6">
-                  <img src={logo} alt="Logo" />
+                  <img src={logo} alt="Logo"/>
                 </div>
                 <h2 className="mb-2 text-center text-3xl leading-9 font-extrabold text-gray-900">
                   Log in to your account
@@ -68,15 +78,15 @@ const Login = () => {
                 {() => (
                   <Form className="mt-8">
                     {loginSuccess && (
-                      <FormSuccess text={loginSuccess} />
+                      <FormSuccess text={loginSuccess}/>
                     )}
                     {loginError && (
-                      <FormError text={loginError} />
+                      <FormError text={loginError}/>
                     )}
                     <div>
                       <div className="mb-2">
                         <div className="mb-1">
-                          <Label text="Email" />
+                          <Label text="Email"/>
                         </div>
                         <FormInput
                           ariaLabel="Email"
@@ -87,7 +97,7 @@ const Login = () => {
                       </div>
                       <div>
                         <div className="mb-1">
-                          <Label text="Password" />
+                          <Label text="Password"/>
                         </div>
                         <FormInput
                           ariaLabel="Password"
