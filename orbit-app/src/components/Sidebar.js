b750220b3c308f242114bbeef7ad6041ef5e0c1e
holomdev <holom.dev@gmail.com>
2021-04-12 22:27:02 -0500

    Conditionally Display Sidebar Items

--- orbit-app/src/components/Sidebar.js
+++ orbit-app/src/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classNames from 'classnames';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
   faChartLine,
   faAddressCard,
@@ -8,38 +8,44 @@ import {
   faCogs,
   faDoorOpen
 } from '@fortawesome/free-solid-svg-icons';
-import { Link, useLocation } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import logo from './../images/logo.png';
+import {AuthContext} from "../context/AuthContext";
 
 const navItems = [
   {
     label: 'Dashboard',
     path: 'dashboard',
-    icon: faChartLine
+    icon: faChartLine,
+    allowedRoles: ['user', 'admin']
   },
   {
     label: 'Inventory',
     path: 'inventory',
-    icon: faChartPie
+    icon: faChartPie,
+    allowedRoles: ['admin']
   },
   {
     label: 'Account',
     path: 'account',
-    icon: faAddressCard
+    icon: faAddressCard,
+    allowedRoles: ['user', 'admin']
   },
   {
     label: 'Settings',
     path: 'settings',
-    icon: faCogs
+    icon: faCogs,
+    allowedRoles: ['user', 'admin']
   },
   {
     label: 'Users',
     path: 'users',
-    icon: faDoorOpen
+    icon: faDoorOpen,
+    allowedRoles: ['admin']
   }
 ];
 
-const NavItem = ({ navItem }) => {
+const NavItem = ({navItem}) => {
   const location = useLocation();
   const isCurrentRoute =
     location.pathname === `/${navItem.path}`;
@@ -52,7 +58,7 @@ const NavItem = ({ navItem }) => {
     <Link to={navItem.path} className={classes}>
       <div className="flex items-center">
         <div className="mr-0 sm:mr-4">
-          <FontAwesomeIcon icon={navItem.icon} />
+          <FontAwesomeIcon icon={navItem.icon}/>
         </div>
         <span className="hidden sm:block">
           {navItem.label}
@@ -62,21 +68,27 @@ const NavItem = ({ navItem }) => {
   );
 };
 
-const NavItemContainer = ({ children }) => (
+const NavItemContainer = ({children}) => (
   <div>{children}</div>
 );
 
 const Sidebar = () => {
+  const authContext = useContext(AuthContext);
+  const {role} = authContext.authState.userInfo;
   return (
     <section className="h-screen">
       <div className="w-16 sm:w-24 m-auto">
-        <img src={logo} rel="logo" alt="Logo" />
+        <img src={logo} rel="logo" alt="Logo"/>
       </div>
       <div className="mt-20">
         {navItems.map((navItem, i) => (
-          <NavItemContainer key={i}>
-            <NavItem navItem={navItem} />
-          </NavItemContainer>
+          <>
+            {navItem.allowedRoles.includes(role) && (
+              <NavItemContainer key={i}>
+                <NavItem navItem={navItem}/>
+              </NavItemContainer>
+            )}
+          </>
         ))}
       </div>
     </section>
