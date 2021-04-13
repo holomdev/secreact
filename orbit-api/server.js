3cb73f77d5505df441e746e87e0fdbd53d248876
holomdev <holom.dev@gmail.com>
2021-04-13 13:00:04 -0500

    Limit Access to Admin Users

--- orbit-api/server.js
+++ orbit-api/server.js
@@ -162,6 +162,17 @@ const checkJwt = jwt({
   audience: 'api.orbit'
 })
 
+const requiredAdmin = (req, res, next) => {
+  const {role} = req.user;
+  if (role !== 'admin') {
+    res.status(401).json({
+      message: 'Insufficient role'
+    })
+  }
+
+  next();
+}
+
 app.get('/api/dashboard-data', checkJwt, (req, res) => {
   console.log(req.user);
   return res.json(dashboardData)
@@ -190,7 +201,7 @@ app.patch('/api/user-role', async (req, res) => {
   }
 });
 
-app.get('/api/inventory', async (req, res) => {
+app.get('/api/inventory', checkJwt, requiredAdmin, async (req, res) => {
   try {
     const inventoryItems = await InventoryItem.find();
     res.json(inventoryItems);
@@ -199,7 +210,7 @@ app.get('/api/inventory', async (req, res) => {
   }
 });
 
-app.post('/api/inventory', async (req, res) => {
+app.post('/api/inventory', checkJwt, requiredAdmin, async (req, res) => {
   try {
     const inventoryItem = new InventoryItem(req.body);
     await inventoryItem.save();
@@ -215,7 +226,7 @@ app.post('/api/inventory', async (req, res) => {
   }
 });
 
-app.delete('/api/inventory/:id', async (req, res) => {
+app.delete('/api/inventory/:id', checkJwt, requiredAdmin, async (req, res) => {
   try {
     const deletedItem = await InventoryItem.findOneAndDelete(
       {_id: req.params.id}
