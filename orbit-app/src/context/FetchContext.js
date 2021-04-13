397a0da7ac1e6ac20bcbe05143e424d560e9104d
holomdev <holom.dev@gmail.com>
2021-04-13 10:00:01 -0500

    Add an HTTP Interceptor to Axios

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,13 +1,26 @@
-import React, { createContext } from 'react';
+import React, {createContext, useContext} from 'react';
 import axios from 'axios';
+import {AuthContext} from "./AuthContext";
 
 const FetchContext = createContext();
-const { Provider } = FetchContext;
+const {Provider} = FetchContext;
 
-const FetchProvider = ({ children }) => {
+const FetchProvider = ({children}) => {
+  const authContext = useContext(AuthContext);
+
   const authAxios = axios.create({
     baseURL: process.env.REACT_APP_API_URL
   });
+
+  authAxios.interceptors.request.use(
+    config => {
+      config.headers.Authorization = `Bearer ${authContext.authState.token}`;
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  )
 
   return (
     <Provider
@@ -20,4 +33,4 @@ const FetchProvider = ({ children }) => {
   );
 };
 
-export { FetchContext, FetchProvider };
+export {FetchContext, FetchProvider};
